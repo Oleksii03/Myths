25de49c8f9ee6aad8df5288a5119536fe3e776f1
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-12-25 16:07:06 +0200

    addJs

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const headerWrapp = document.querySelector('.js-header-wrapper');
+const headerContainer = document.querySelector('.js-header-container').clientHeight;
+
+headerWrapp.style.height = headerContainer + 'px';
