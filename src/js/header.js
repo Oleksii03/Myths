fbdb35b4596e21726616899dc1916e483b19c162
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-12-26 16:04:52 +0200

    addTabMenuJs

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,16 @@
 const headerWrapp = document.querySelector('.js-header-wrapper');
 const headerContainer = document.querySelector('.js-header-container').clientHeight;
+const tabMenu = document.querySelector('.js-tab-menu');
+const dropdown = document.querySelector('.js-tab-list');
 
-headerWrapp.style.height = headerContainer + 'px';
+(() => headerWrapp.style.height = headerContainer + 'px')();
+
+tabMenu.addEventListener('click', (e) => {
+  const { target } = e;
+
+  if (target.classList.contains('dropdown')) {
+    dropdown.classList.toggle('is-hidden');
+    return;
+  }
+
+});
