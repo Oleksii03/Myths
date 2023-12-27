9a9dca3abdfd2bf19eac2f165b8f88ddea197103
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-12-27 19:44:22 +0200

    addJsMobMenu

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,12 @@
 const headerWrapp = document.querySelector('.js-header-wrapper');
 const headerContainer = document.querySelector('.js-header-container').clientHeight;
+
 const tabMenu = document.querySelector('.js-tab-menu');
 const dropdown = document.querySelector('.js-tab-list');
+
+const mobMenuBtn = document.querySelector('.js-header-btm-menu');
+const mobMenuBtnClose = document.querySelector('.js-btn-close-menu');
+const mobMenu = document.querySelector('.js-mob-menu');
 
 (() => headerWrapp.style.height = headerContainer + 'px')();
 
@@ -14,3 +19,16 @@ tabMenu.addEventListener('click', (e) => {
   }
 
 });
+
+// -------mobMenu----------------------------
+
+mobMenuBtn.addEventListener('click', onOpenMobMenu);
+mobMenuBtnClose.addEventListener('click', onCloseMobMenu);
+
+function onOpenMobMenu (e) {
+  mobMenu.classList.remove('is-hidden-mob-menu');
+}
+
+function onCloseMobMenu (e) {
+  mobMenu.classList.add('is-hidden-mob-menu');
+}
