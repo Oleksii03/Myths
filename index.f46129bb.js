6e9f24196b97db1151f8456eb8420dc0ebb06cbf
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2023-12-27 17:44:49 +0000

    Deploying to gh-pages from @ Oleksii03/Myths@9a9dca3abdfd2bf19eac2f165b8f88ddea197103 🚀

--- index.f46129bb.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".js-header-wrapper"),t=document.querySelector(".js-header-container").clientHeight,c=document.querySelector(".js-tab-menu"),s=document.querySelector(".js-tab-list");e.style.height=t+"px",c.addEventListener("click",(e=>{const{target:t}=e;t.classList.contains("dropdown")&&s.classList.toggle("is-hidden")}));
-//# sourceMappingURL=index.f46129bb.js.map
