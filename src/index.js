c2007fb86e9e27efebcef42b93163b375999ae5e
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-12-30 21:13:22 +0200

    aadSlider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 import './js/header';
-import './js/splide-famous';
--- src/js/slide-famous.js
+++ src/js/slide-famous.js
@@ -1,7 +1,6 @@
 import '../../node_modules/@splidejs/splide/dist/css/splide-core.min.css';
 
 import Splide from '@splidejs/splide';
-// import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
 
 const splide = new Splide('.splide', {
   type: 'loop',
