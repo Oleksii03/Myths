6cbe2a149b8f9de0bf7065e251407b9e25edd3ea
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-01-09 21:50:19 +0200

    add-Js-backdrop-search

---
+++ src/js/backdrop-search.js
@@ -0,0 +1,19 @@
+const backdropSearch = document.querySelector('.js-backdrop-search');
+const btnSearch = document.querySelector('.js-search-icon');
+
+btnSearch.addEventListener('click', onOpenBackdropSearch);
+backdropSearch.addEventListener('click', onCloseBackdropSearch);
+
+function onOpenBackdropSearch (e) {
+
+  backdropSearch.classList.remove('backdrop-search--hidden');
+
+}
+
+function onCloseBackdropSearch (e) {
+  const { target, currentTarget } = e;
+
+  if (target !== currentTarget) return;
+
+  backdropSearch.classList.add('backdrop-search--hidden');
+}
