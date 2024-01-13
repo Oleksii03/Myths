b9ed5696e7067f57bda5c026f3ff2ee82994dbbf
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-01-13 15:14:31 +0200

    add-witcher-card

---
+++ src/js/data/bestiary-witcher.js
@@ -0,0 +1,21 @@
+
+export const bestiaryWitcher = [
+  {
+    name: "Боровик",
+    description: `Боровик (пол. Borowy, англ. Spriggan) — бестія у всесвіті Відьмака ...`,
+    image: "../../images/beasts-witcher/spriggan.jpg",
+    genus: "Релікти",
+  },
+  {
+    name: "Грифон",
+    description: `Грифон — це істота з тілом, задніми ногами та хвостом лева і головою ...`,
+    image: "https://github.com/Oleksii03/Myths/blob/main/src/images/beasts-main/item-mob-2.jpg?raw=true",
+    genus: "Гібриди",
+  },
+  {
+    name: "Домовик",
+    description: `Домовик, гріг — це крихітний гуманоїд, пустотлива, але нешкідлива істота ...`,
+    image: "https://github.com/Oleksii03/Myths/blob/main/src/images/beasts-main/item-mob-3.jpg?raw=true",
+    genus: "Людиноподібні",
+  },
+];
