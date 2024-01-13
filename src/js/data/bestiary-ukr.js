9d17b4268566009cb94bed671e7e0699a326a3ec
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-01-13 14:31:49 +0200

    add-js-card

--- src/js/data/bestiary-ukr.js
+++ src/js/data/bestiary-ukr.js
@@ -3,16 +3,16 @@ export const bestiaryUkr = [
     name: "Боровик",
     description: `Боровик — (за слов'янською міфологією) один
     з лісових духів, господар борів та`,
-    image: "./images/beasts-main/item-mob-1.jpg",
+    image: "https://github.com/Oleksii03/Myths/blob/main/src/images/beasts-main/item-mob-1.jpg?raw=true",
   },
   {
     name: "Грифон (Гриф)",
     description: `Грифони — суперечливі істоти, які одночасно об'єднують Небо і Землю ...`,
-    image: "./images/beasts-main/item-mob-2.jpg",
+    image: "https://github.com/Oleksii03/Myths/blob/main/src/images/beasts-main/item-mob-2.jpg?raw=true",
   },
   {
     name: "Домовик",
     description: `Домовик  — у слов'янських народів домашній дух, міфологічний господар ...`,
-    image: "./images/beasts-main/item-mob-3.jpg",
+    image: "https://github.com/Oleksii03/Myths/blob/main/src/images/beasts-main/item-mob-3.jpg?raw=true",
   },
 ];
