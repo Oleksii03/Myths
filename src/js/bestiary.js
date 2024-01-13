d204f9d6c68fda4dd506d2a2715384311e416879
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-01-13 19:24:09 +0200

    fix

--- src/js/bestiary.js
+++ src/js/bestiary.js
@@ -24,10 +24,6 @@ function createMarkupBestiaryListUkr (data) {
 
     <div class="bestiary-item__heder">
 
-      <!-- <svg width="40" height="40" class="list-beasts__logo bestiary-item__logo">
-        <use href="./images/sprite.svg#icon-logo-ua"></use>
-      </svg> -->
-
       <svg class="list-beasts__logo bestiary-item__logo" xmlns="http://www.w3.org/2000/svg" width="40" height="40"
         viewBox="0 0 40 40" fill="none">
         <circle cx="20" cy="20" r="20" fill="#005AC2" />
@@ -98,8 +94,7 @@ function createMarkupBestiaryListUkr (data) {
 
 // -----/-onCreateMarkupUkr-----------------------
 
-
-// ------onCreateMarkupWitcher-----------------------
+// ------onCreateMarkupWitcher---------------------
 
 mythBtnWitcher.addEventListener('click', onCreateMarkupWitcher);
 
@@ -186,3 +181,5 @@ function createMarkupBestiaryListWitcher (data) {
   }).join('');
 };
 // -----/-onCreateMarkupWitcher-----------------------
+
+// bestiaryList.innerHTML = `<li>hello<img class="list-beasts__img" src='//images/beasts-main/item-mob-1.jpg" alt="боровик'></li>`;
