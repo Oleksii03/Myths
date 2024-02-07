fb11f763ec1ae9c5716b707d31004b4df39d2fd5
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-02-07 15:56:35 +0200

    add-griff

--- src/js/page-griffin/griffin.js
+++ src/js/page-griffin/griffin.js
@@ -6,6 +6,8 @@ import griffin1 from '../../images/beasts-main/item-mob-2.jpg';
 import griffin2 from '../../images/page-griffin/item-img-2.jpg';
 import griffin3 from '../../images/page-griffin/item-img-3.jpg';
 
+import griffinWihc from '../../images/beasts-witcher/grifon.jpg';
+
 // ---------refs-------------------------------
 
 const mythBtnUkrEl = document.querySelector('.js-myth-btns_ukr-griff');
@@ -123,7 +125,7 @@ function createGalleryGriffinUkr () {
           <img data-id="2" class="gallery-borovik__img" src="${griffin2}" alt="griffin-2">
         </li>
         <li class="gallery-borovik__item gallery-borovik__item-row">
-          <img data-id="3" class="gallery-borovik__img gallery-borovik__img-row" src="${griffin3}"
+          <img data-id="3" class="gallery-borovik__img gallery-borovik__img-row gallery--griffin" src="${griffin3}"
             alt="griffin-3">
         </li>
       </ul>
@@ -131,8 +133,137 @@ function createGalleryGriffinUkr () {
       <button class="gallery-borovik__btn js-gallery-btn-wich" type="button">У грі Відьмак</button>`;
 };
 
+// -----/--onCreateMarcupUkrGriff----------------------------------
 
 function onCreateMarcupWitcherGriff (e) {
   mythBtnUkrEl.classList.remove('myth-btns__active');
   mythBtnWitcherEl.classList.add('myth-btns__active');
+
+  mainContentGriffin.innerHTML = createMarcupGriffWitcher();
+  galleryGriffin.innerHTML = '';
 }
+
+function createMarcupGriffWitcher () {
+  return `
+  <div class="bestiary-list__item borovik-item">
+        <div class="borovik-item__heder">
+          <svg width="40" height="40" class="borovik-item__logo">
+            <use href="${svg + '#witcher-icon'}"></use>
+          </svg>
+
+          <div class="borovik-item__content">
+            <h3 class="borovik-item__title">Грифон (Гриф)</h3>
+            <h4 class="borovik-item__sub-title">Гра «Відьмак 3: Дикий Гін»</h4>
+          </div>
+
+          <a href="#" class="borovik-item__svg-more">
+            <svg width="18" height="18" class="borovik-body__logo">
+              <use href="${svg + '#more-vertical'}"></use>
+            </svg>
+          </a>
+        </div>
+
+        <picture class="borovik-item__picture">
+          <a href="#">
+            <img class="borovik-item__img borovik-item__img-witcher" src="${griffinWihc}" alt="spriggan"
+              loading="lazy">
+          </a>
+        </picture>
+
+        <div class="borovik-item__body borovik-body">
+
+          <div class="author-img">
+            <h5 class="author-img__title">Анна Подедворна</h5>
+            <a class="author-img__link" href="#">Авторка малюнку</a>
+          </div>
+
+          <a class="borovik-body__link-myth" href="#">
+            <svg width="18" height="18" class="borovik-body__logo">
+              <use href="${svg + '#leading'}"></use>
+            </svg>
+            Гібриди
+          </a>
+
+          <p class="borovik-body__text">Грифон - це істота з тілом, задніми ногами та хвостом лева і головою, крилами та пазурами орла.</p>
+
+
+          <table class="borovik-witcher__table">
+            <tr class="borovik-witcher__row">
+              <td class="borovik-witcher__data">Клас</td>
+              <td class="borovik-witcher__data">Гібриди</td>
+            </tr>
+
+            <tr class="borovik-witcher__row">
+              <td class="borovik-witcher__data">Місце появи/
+                <br> Проживання
+              </td>
+              <td class="borovik-witcher__data">Білий Сад<br>
+              Велен<br>
+              Скелліґе<br>
+              Оксенфурт</td>
+            </tr>
+
+            <tr class="borovik-witcher__row">
+              <td class="borovik-witcher__data">Стійкість</td>
+              <td class="borovik-witcher__data">До оглушення</td>
+            </tr>
+
+            <tr class="borovik-witcher__row">
+              <td class="borovik-witcher__data">Вразливість</td>
+              <td class="borovik-witcher__data">
+                  «Картеч» (бомба)<br>
+                  Мастило проти гібридів<br>
+                  знак Аард<br>
+                  знак Квен
+              </td>
+            </tr>
+
+            <tr class="borovik-witcher__row">
+              <td class="borovik-witcher__data">Тактика</td>
+              <td class="borovik-witcher__data">
+              Налетіти на жертву у стрімкому піке і вдарити кігтями
+              </td>
+            </tr>
+
+            <tr class="borovik-witcher__row">
+              <td class="borovik-witcher__data">Трофей</td>
+              <td class="borovik-witcher__data">Так</td>
+            </tr>
+          </table>
+
+        </div>
+      </div>
+      
+      <ol class="main-content__list">
+        <li class="main-content__list-item">
+          <h3 class="main-content__list-title">Зміст</h3>
+        </li>
+        <li class="main-content__list-item">1. Опис, Запис у бестіарії</li>
+        <li class="main-content__list-item">2. Галерея</li>
+        <li class="main-content__list-item">3. Відео</li>
+      </ol>
+
+      <div class="main-content__description description-content">
+
+        <h4 class="description-content__title">Гра «Відьмак 3: Дикий Гін»,
+          <br> Запис у бестіарії
+        </h4>
+
+        <p class="description-content__text">
+          Боровики — підвид лісовиків. Натрапити на них можна в недоступних безлюдних хащах. Якщо їх не злити, вони
+          можуть і не напасти, але в гніві ці істоти страшні. Боровик дуже кремезний, завдає величезної шкоди, а м’яса
+          зжерти може більше, ніж на доброму весіллі подають.
+        </p>
+        <p class="description-content__text">
+          Кожний боровик панує над рослинами й нерозривно пов’язаний із ними. Якщо хтось нищить зелень у володіннях
+          боровика, потвора обов’язково прийде й покарає порушника спокою.
+        </p>
+        <p class="description-content__text">
+          У бою боровик управно користується своїми дужими кінцівками. Атакує він стрімко, що, беручи до уваги його
+          масивність, може спричинити клопіт. Подібна до кори тверда шкіра боровика витримує навіть сильні вдари мечем,
+          а сам він б’є так потужно, що ставити блок не варто й пробувати. Слід пильнувати коріння, яке боровик примушує
+          вилазити з-під землі й хапати ворога. Ця потвора вміє також телепортуватися, раптово з’являючись просто перед
+          супротивником. Бити його Знаком Аард марно, бо жодна сила цього монстра не повалить.
+        </p>
+      </div>`;
+};
