a3c9ccea9f932c31741d3a52fa9458d1bbba1c82
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-02-21 20:48:30 +0200

    add

--- src/js/backdrop-search.js
+++ src/js/backdrop-search.js
@@ -2,20 +2,20 @@ const backdropSearch = document.querySelector('.js-backdrop-search');
 const btnSearch = document.querySelector('.js-search-icon');
 
 btnSearch.addEventListener('click', onOpenBackdropSearch);
-backdropSearch.addEventListener('click', onCloseBackdropSearch);
+// backdropSearch.addEventListener('click', onCloseBackdropSearch);
 
 function onOpenBackdropSearch (e) {
-  backdropSearch.classList.remove('backdrop-search--hidden');
+  backdropSearch.classList.toggle('backdrop-search--hidden');
 
   document.body.style.overflow = document.body.style.overflow ? '' : 'hidden';
 }
 
-function onCloseBackdropSearch (e) {
-  const { target, currentTarget } = e;
+// function onCloseBackdropSearch (e) {
+//   const { target, currentTarget } = e;
 
-  if (target !== currentTarget) return;
+//   if (target !== currentTarget) return;
 
-  backdropSearch.classList.add('backdrop-search--hidden');
+//   backdropSearch.classList.add('backdrop-search--hidden');
 
-  document.body.style.overflow = document.body.style.overflow ? '' : 'hidden';
-}
+//   document.body.style.overflow = document.body.style.overflow ? '' : 'hidden';
+// }
