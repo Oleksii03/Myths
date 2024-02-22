4e666247ee80d2b2d4b54bacd407e54137bc78ca
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-02-22 20:22:10 +0200

    finish-page-characters

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,9 +1,15 @@
+import { charactersUkr } from "./data/characters-ukr";
+import { charactersWitcher } from "./data/characters-ukr";
 
+console.log(charactersWitcher);
+
+import svg from '../images/sprite.svg';
 // -----refs ----------------------------
 const btnWitcher = document.querySelector('.characters-btn__witcher');
 const btnUkr = document.querySelector('.jscharacters-btn-ukr');
 const btnBox = document.querySelector('.js-characters-btn-box');
 
+const cardList = document.querySelector('.js-characters-section-list');
 // ----/-refs ---------------------------
 
 btnUkr.addEventListener('click', onCreateContentUkr);
@@ -16,16 +22,111 @@ function onCreateContentUkr (e) {
 
   btnUkr.classList.add('characters-btn__active');
   btnWitcher.classList.remove('characters-btn__active');
+
+  cardList.innerHTML = createCardMarkupUkr(charactersUkr);
 }
 
+
+function createCardMarkupUkr (dataCard) {
+  return dataCard.map(({ name, genus, img, description, ref }) => {
+    return `<li class="bestiary-list__item bestiary-item">
+    <div class="bestiary-item__heder">
+        <svg width="40" height="40" class="bestiary-item__logo">
+          <use href="${svg + '#icon-ukr-characters'}"></use>
+        </svg> 
+
+      <div class="bestiary-item__content">
+        <h3 class="bestiary-item__title">${name}</h3>
+        <h4 class="bestiary-item__sub-title">Українська міфологія</h4>
+      </div>
+
+      <a href="#" class="bestiary-item__svg-more">
+        <svg width="18" height="18" class="bestiary-body__logo">
+          <use href="${svg + '#more-vertical'}"></use>
+        </svg> 
+      </a>
+    </div>
+
+    <picture class="bestiary-item__picture">
+      <a href="${ref}">
+        <img class="bestiary-item__img bestiary-item__img-ukr" src="${img}" alt="${name}" loading="lazy">
+      </a>
+    </picture>
+
+    <div class="bestiary-item__body bestiary-body">
+
+      <a class="bestiary-body__link-myth" href="#">
+        <svg width="18" height="18" class="bestiary-body__logo">
+          <use href="${svg + '#icon-profile'}"></use>
+        </svg> 
+        ${genus}
+      </a>
+
+      <p class="bestiary-body__text">${description}</p>
+
+      <a class="bestiary-body__link-btn" href="${ref}">
+        Читати далі
+        <svg width="19" height="18" class="bestiary-item__logo">
+          <use href="${svg + '#arrow-up-right'}"></use>
+        </svg> 
+      </a>
+    </div>
+  </li>`;
+  }).join('');
+}
 
 function onCreateContentWitcher (e) {
   btnBox.classList.add('characters-btn__active');
 
   btnUkr.classList.remove('characters-btn__active');
   btnWitcher.classList.add('characters-btn__active');
+
+  cardList.innerHTML = createCardMarkupWitcher(charactersWitcher);
 }
 
 
+function createCardMarkupWitcher (dataCard) {
+  return dataCard.map(({ name, genus, img, description, ref, logo }) => {
+    return `<li class="bestiary-list__item bestiary-item">
+    <div class="bestiary-item__heder">
+        <svg width="40" height="40" class="bestiary-item__logo">
+          <use href="${svg + logo}"></use>
+        </svg> 
+      <div class="bestiary-item__content">
+        <h3 class="bestiary-item__title">${name}</h3>
+        <h4 class="bestiary-item__sub-title">Гра «Відьмак 3: Дикий Гін»</h4>
+      </div>
 
+      <a href="#" class="bestiary-item__svg-more">
+        <svg width="18" height="18" class="bestiary-body__logo">
+          <use href="${svg + '#more-vertical'}"></use>
+        </svg> 
+      </a>
+    </div>
 
+    <picture class="bestiary-item__picture">
+      <a href="${ref}">
+        <img class="bestiary-item__img bestiary-item__img-witcher js-bestiary-item-img-witcher" src="${img}" alt="${name}" loading="lazy">
+      </a>
+    </picture>
+
+    <div class="bestiary-item__body bestiary-body">
+      <a class="bestiary-body__link-myth" href="#">
+        <svg width="18" height="18" class="bestiary-item__logo">
+          <use href="${svg + '#icon-profile'}"></use>
+        </svg> 
+        ${genus}
+      </a>
+
+      <p class="bestiary-body__text">${description}</p>
+
+      <a class="bestiary-body__link-btn" href="${ref}">
+        Читати далі
+        <svg width="19" height="18" class="bestiary-item__logo">
+          <use href="${svg + '#arrow-up-right'}"></use>
+        </svg> 
+      </a>
+    </div>
+  </li>`;
+  }).join('');
+}
---
+++ src/js/data/characters-ukr.js
@@ -0,0 +1,41 @@
+import kozak from '../../images/characters/ukrainskii-kozak-1.jpeg';
+import witch from '../../images/characters/white-whitch-1.png';
+
+import Geralt from '../../images/characters/Geralt.jpg';
+import Yennefer from '../../images/characters/Yennefer.jpg';
+
+export const charactersUkr = [
+  {
+    name: 'Козак-характерник',
+    genus: 'Козаки-характерники',
+    img: kozak,
+    description: `Відьма́к та Характе́рник (відьма́н) — персонаж слов'янської міфології, чаклун ...`,
+    ref: '#',
+  },
+  {
+    name: 'Чародійки',
+    genus: 'Чаклунки/Відьми',
+    img: witch,
+    description: `Ві́дьма, чаклу́нка (чародійка) — жінка, наділена чаклунськими здібностями ...`,
+    ref: '#',
+  },
+];
+
+export const charactersWitcher = [
+  {
+    name: 'Ґеральт з Рівії',
+    genus: 'Відтмак',
+    img: Geralt,
+    logo: '#witcher-icon',
+    description: `Ґеральт з Рівії, «Білий Вовк» — головний герой літературного циклу романів ...`,
+    ref: '#',
+  },
+  {
+    name: 'Йеннефер',
+    genus: 'Чародійка',
+    img: Yennefer,
+    logo: '#icon-logo-yennefer',
+    description: `Йеннефер — одна з головних героїнь літературного циклу А.Сапковського ...`,
+    ref: '#',
+  },
+];
