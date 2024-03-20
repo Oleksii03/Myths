f99c576cddb63df65be19a1eed1b4946fe42bcfb
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-03-20 22:21:01 +0200

    add-js-s

--- src/js/backdrop-search.js
+++ src/js/backdrop-search.js
@@ -1,8 +1,14 @@
+import { bestiaryUkr } from "./data/bestiary-ukr";
+import { bestiaryWitcher } from "./data/bestiary-witcher";
+
 const backdropSearch = document.querySelector('.js-backdrop-search');
 const btnSearch = document.querySelector('.js-search-icon');
 
+const searchList = document.querySelector('.js-search-list');
+
+console.log(searchList);
+
 btnSearch.addEventListener('click', onOpenBackdropSearch);
-// backdropSearch.addEventListener('click', onCloseBackdropSearch);
 
 function onOpenBackdropSearch (e) {
   backdropSearch.classList.toggle('backdrop-search--hidden');
@@ -10,12 +16,7 @@ function onOpenBackdropSearch (e) {
   document.body.style.overflow = document.body.style.overflow ? '' : 'hidden';
 }
 
-// function onCloseBackdropSearch (e) {
-//   const { target, currentTarget } = e;
 
-//   if (target !== currentTarget) return;
+const allСharacters = [...bestiaryUkr, ...bestiaryWitcher];
 
-//   backdropSearch.classList.add('backdrop-search--hidden');
-
-//   document.body.style.overflow = document.body.style.overflow ? '' : 'hidden';
-// }
+allСharacters.filter(el => el.name.toLocaleLowerCase() === 'боровик').map(el => console.log(el));
